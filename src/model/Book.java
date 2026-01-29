fb02585a2e05da29a2725d805b6e058f5376acf3
Niharika <niharikatanwar067@gmail.com>
2026-01-29 17:48:59 +0530

    Added Book model

---
+++ src/model/Book.java
@@ -0,0 +1,13 @@
+public class Book {
+    String title;
+    String author;
+    String genre;
+    boolean available;
+
+    public Book(String title, String author, String genre) {
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.available = true;
+    }
+}
