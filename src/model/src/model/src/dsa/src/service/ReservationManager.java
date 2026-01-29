ed8620621db39c08838c98fb2310d2a8468c56b4
Niharika <niharikatanwar067@gmail.com>
2026-01-29 17:51:48 +0530

    Added priority-based reservation manager

---
+++ src/model/src/model/src/dsa/src/service/ReservationManager.java
@@ -0,0 +1,24 @@
+import java.util.PriorityQueue;
+
+public class ReservationManager {
+
+    private PriorityQueue<User> waitingQueue;
+
+    public ReservationManager() {
+        waitingQueue = new PriorityQueue<>(
+            (u1, u2) -> u2.priority - u1.priority
+        );
+    }
+
+    public void addToQueue(User user) {
+        waitingQueue.add(user);
+    }
+
+    public User getNextUser() {
+        return waitingQueue.poll();
+    }
+
+    public boolean hasWaitingUsers() {
+        return !waitingQueue.isEmpty();
+    }
+}
