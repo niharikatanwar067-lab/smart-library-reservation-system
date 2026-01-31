de8cd509314d916a8df759068306695f36dcb019
Niharika <niharikatanwar067@gmail.com>
2026-01-31 13:40:50 +0530

    Update User.java

--- src/model/User.java
+++ src/model/User.java
@@ -1,11 +1,19 @@
 package model;
 
 public class User {
-    String name;
-    int priority;
+    private String name;
+    private int priority;
 
     public User(String name, int priority) {
         this.name = name;
         this.priority = priority;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPriority() {
+        return priority;
     }
 }
