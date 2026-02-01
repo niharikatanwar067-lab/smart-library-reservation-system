09c3cdb12bccd8d96a5c2eb55f3c3211a8852222
Niharika <niharikatanwar067@gmail.com>
2026-02-01 13:43:00 +0530

    Update LibraryService.java

--- src/service/LibraryService.java
+++ src/service/LibraryService.java
@@ -18,6 +18,14 @@ public class LibraryService {
         System.out.println("User registered: " + user.getName());
     }
 
+    public void searchBook(String keyword) {
+    System.out.println("Search results for \"" + keyword + "\":");
+    books.values().stream()
+            .filter(b -> b.getTitle().toLowerCase().contains(keyword.toLowerCase()))
+            .forEach(b -> System.out.println("- " + b.getTitle()));
+    }
+
+
     public void issueBook(String title, String userName) {
         Book book = books.get(title.toLowerCase());
         User user = users.get(userName);
@@ -35,10 +43,26 @@ public class LibraryService {
         }
     }
 
+    public void returnBook(String title) {
+        Book book = books.get(title.toLowerCase());
+
+        if (book != null && !book.isAvailable()) {
+            book.setAvailable(true);
+            System.out.println("\"" + book.getTitle() + "\" returned successfully.");
+        } else {
+            System.out.println("Invalid return attempt.");
+        }
+    }
+
+
     public void showRecommendations(String userName) {
         System.out.println("Recommendations for " + userName + ":");
         books.values().stream()
                 .filter(Book::isAvailable)
                 .forEach(b -> System.out.println("- " + b.getTitle()));
+        }
     }
-}
+
+    public void showMostDemandedBook() {
+        System.out.println("Most demanded books feature coming soon (analytics module).");
+    }
